d66337c11f52337e01f201d9d02841964ad10b86
ziaul-hoque4820 <ziaulhoque4820@gmail.com>
2025-11-26 22:45:34 +0600

    create Get and Post API for parcels

--- index.js
+++ index.js
@@ -16,28 +16,48 @@ const uri = `mongodb+srv://${process.env.ZAPSHIFT_DB_USER}:${process.env.ZAPSHIF
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+        // data collection
+        const parcelCollection = client.db('parcelDB').collection('parcels');
+
+        // Parcel API
+        app.get('/parcels', async (req, res) => {
+            const parcels = await parcelCollection.find().toArray();
+            res.send(parcels);
+        });
+
+        app.post('/parcels', async (req, res) => {
+            try {
+                const newParcel = req.body;
+
+                const result = await parcelCollection.insertOne(newParcel)
+                res.status(201).send(result)
+            } catch (error) {
+                console.log('Error inserting parcel:', error);
+                res.status(500).send({ message: "Failed to create parcel" })
+            }
+        });
 
 
 
-    
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
