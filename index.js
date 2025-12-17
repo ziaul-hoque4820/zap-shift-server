f3d2925fc9a2745c47b37852172d272d051f9fe4
ziaul-hoque4820 <ziaulhoque4820@gmail.com>
2025-12-17 18:49:33 +0600

    update localhost port

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 const admin = require("firebase-admin");
 
 //middleware
